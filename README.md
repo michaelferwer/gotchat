6f51e4bb0b983dc066797f756525a2806eb64525
Michael Werner <michael.werner-ext@cloudwatt.com>
2015-10-09 17:27:53 +0200

    Change of dependecy manager to GB

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # gotchat
-Project in Go
+Project in GO
+
+README in progress
+
+
+## Project Manager and Dependency Manager
+
+This project use GB and GB Vendor to manage project and dependencies.
+
+Documentation on GB can be find on this website : http://getgb.io/
+
+For more detail on GB Vendor look at https://godoc.org/github.com/constabulary/gb/cmd/gb-vendor
+
+### Quick install
+
+To use GB, we must execute the following command to set the location of the current project.
+```
+export GB_PROJECT_DIR=`pwd`
+```
--- src/com.werner/gotchat/gotchat.go
+++ src/com.werner/gotchat/gotchat.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+	"github.com/apsdehal/go-logger"
 )
 
+func handleConnection (w http.ResponseWriter, r *http.Request) {
+	log := getLogger()
+	log.Debug("Connection")
+	w.Header().Set("Content-Type", "plain/text; charset=utf-8")
+	w.Write([]byte("Hello World"))
+	return
+}
+
+func getLogger () *logger.Logger{
+	log, err := logger.New("LOG", 1, os.Stdout)
+	if err != nil {
+		panic(err) // TODO Check for error
+	}
+	return log
+}
+
 func main () {
-	fmt.Printf("Hello World")
+	http.HandleFunc("/", handleConnection)
+  http.ListenAndServe(":8080", nil)
 }

e5ffe0ba68d8377902d86069efbd2c85fd9b8eba
Michael Werner <michael.werner-ext@cloudwatt.com>
2016-01-26 17:57:01 +0100

    Added resources to test websocket

--- src/com.werner/gotchat/gotchat.go
+++ src/com.werner/gotchat/gotchat.go
@@ -3,12 +3,44 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 	"github.com/apsdehal/go-logger"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+func main() {
+	router := gin.Default()
+	router.GET("/version", version)
+	router.GET("/ws/echo", func(c *gin.Context) {
+        echo(c.Writer, c.Request)
+    })
+	router.Run(":8080")
+}
 
 func version(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{ "version": "1.0.0" })
+}
+
+var wsupgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+func echo(w http.ResponseWriter, r *http.Request){
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	    if err != nil {
+	        fmt.Printf("Failed to set websocket upgrade: %+v \n", err)
+	        return
+	    }
+
+	    for {
+	        t, msg, err := conn.ReadMessage()
+	        if err != nil {
+	            break
+	        }
+	        conn.WriteMessage(t, msg)
+	    }
 }
 
 func getLogger() *logger.Logger {
@@ -17,10 +49,4 @@ func getLogger() *logger.Logger {
 		panic(err) // TODO Check for error
 	}
 	return log
-}
-
-func main() {
-	router := gin.Default()
-	router.GET("/version", version)
-	router.Run(":8080")
 }

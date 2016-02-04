1bdcebac4957965cb417fa1c58d58e0758ac28b1
Werner Michael <michael.ferwer@gmail.com>
2016-02-04 22:30:45 +0100

    Add input to enter text

--- server/src/com.werner/gotchat/gotchat.go
+++ server/src/com.werner/gotchat/gotchat.go
@@ -1,60 +1,69 @@
 package main
 
 import (
-	"net/http"
-	"os"
-	"log"
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+  "net/http"
+  "os"
+  "log"
+  "sync"
+  "github.com/gin-gonic/gin"
+  "github.com/gorilla/websocket"
 )
 
 var connections map[*websocket.Conn]bool;
+// Used to sync access to map connections
+var mutex = &sync.Mutex{}
 
 var LOGGER *log.Logger;
 
 func main() {
-	LOGGER = log.New(os.Stdout, "INFO: ", log.Ldate | log.Ltime | log.Lshortfile)
-	connections = make(map[*websocket.Conn]bool)
-	router := gin.Default()
-	router.GET("/version", version)
-	router.GET("/ws/echo", func(c *gin.Context) {
-		echo(c.Writer, c.Request)
-	})
-	router.Run(":8080")
+  LOGGER = log.New(os.Stdout, "INFO: ", log.Ldate | log.Ltime | log.Lshortfile)
+  connections = make(map[*websocket.Conn]bool)
+  router := gin.Default()
+  router.GET("/version", version)
+  router.GET("/ws/echo", func(c *gin.Context) {
+    echo(c.Writer, c.Request)
+  })
+  router.Run(":8080")
 }
 
 func version(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"version": "1.0.0" })
+  c.JSON(http.StatusOK, gin.H{"version": "1.0.0" })
 }
 
 var wsupgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
+  CheckOrigin: func(r *http.Request) bool {
+    return true
+  },
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
-	conn, err := wsupgrader.Upgrade(w, r, nil)
-	if err != nil {
-		//noinspection GoPlaceholderCount
-		LOGGER.Printf("Failed to set websocket upgrade: %+v \n", err)
-		return
-	}
-	defer
-	func() {
-		delete(connections, conn)
-		conn.Close()
-	}()
-	connections[conn] = true
-	for {
-		t, msg, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-		for key, _ := range connections {
-			key.WriteMessage(t, msg)
-		}
-	}
+  conn, err := wsupgrader.Upgrade(w, r, nil)
+  if err != nil {
+    //noinspection GoPlaceholderCount
+    LOGGER.Printf("Failed to set websocket upgrade: %+v \n", err)
+    return
+  }
+  defer
+  func() {
+    mutex.Lock()
+    delete(connections, conn)
+    mutex.Unlock()
+    conn.Close()
+  }()
+  mutex.Lock()
+  connections[conn] = true
+  mutex.Unlock()
+  for {
+    t, msg, err := conn.ReadMessage()
+    if err != nil {
+      break
+    }
+    mutex.Lock()
+    for key, _ := range connections {
+      key.WriteMessage(t, msg)
+    }
+    mutex.Unlock()
+  }
 }

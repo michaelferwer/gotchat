fa91d05ece1f7ce8fe25b633c2f512c32a0fcd86
Werner Michael <michael.ferwer@gmail.com>
2016-02-23 22:07:57 +0100

    Create branch

--- server/src/com.werner/gotchat/broadcaster/broadcaster.go
+++ server/src/com.werner/gotchat/broadcaster/broadcaster.go
@@ -6,14 +6,39 @@ import (
 
 type Broadcaster struct {
   connections map[*websocket.Conn]bool
-  broadcastChannel *chan string
+  broadcastChannel *chan Message
 }
 
-func NewBroadCaster() *Broadcaster{
-  return new(Broadcaster)
+func NewBroadCaster(broadcastChannel *chan Message) *Broadcaster{
+  broadcaster := new(Broadcaster)
+  broadcaster.connections = make(map[*websocket.Conn]bool)
+  broadcaster.broadcastChannel = broadcastChannel
+  return broadcaster
+}
+
+type Messenger interface {
+  GetMessage() string
 }
 
 type Message struct {
   connection *websocket.Conn
-
+  messageType MessageType
+  message string
 }
+
+func (m *Message) GetMessage() string {
+  switch m.messageType {
+  case SEND:
+    return m.message
+  default:
+    return nil
+  }
+}
+
+type MessageType int
+
+const (
+  SUBSCRIBE MessageType = iota
+  UNSUBSCRIBE
+  SEND
+)
